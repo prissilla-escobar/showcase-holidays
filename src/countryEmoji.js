dcd208d826b88f2cfca17e7728b41c18f5bf301f
Prissilla Escobar <prissilla988@gmail.com>
2023-09-21 14:34:59 -0600

    Create array of objects that contain the countries image for flags

---
+++ src/countryEmoji.js
@@ -0,0 +1,663 @@
+export const countryFlags =
+[
+    {
+        id: 1,
+        countryCode: "AD",
+        name: "Andorra",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/AD.png"
+    },
+    {
+        id: 2,
+        countryCode: "AL",
+        name: "Albania",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/AL.png"
+    },
+    {
+        id: 3,
+        countryCode: "AR",
+        name: "Argentina",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/AR.png"
+    },
+    {
+        id: 4,
+        countryCode: "AT",
+        name: "Austria",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/AT.png"
+    },
+    {
+        id: 5,
+        countryCode: "AU",
+        name: "Australia",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/AU.png"
+    },
+    {
+        id: 6,
+        countryCode: "AX",
+        name: "Åland Islands",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/AX.png"
+    },
+    {
+        id: 7,
+        countryCode: "BA",
+        name: "Bosnia and Herzegovina",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/BA.png"
+    },
+    {
+        id: 8,
+        countryCode: "BB",
+        name: "Barbados",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/BB.png"
+    },
+    {
+        id: 9,
+        countryCode: "BE",
+        name: "Belgium",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/BE.png"
+    },
+    {
+        id: 10,
+        countryCode: "BG",
+        name: "Bulgaria",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/BG.png"
+    },
+    {
+        id: 11,
+        countryCode: "BJ",
+        name: "Benin",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/BJ.png"
+    },
+    {
+        id: 12,
+        countryCode: "BO",
+        name: "Bolivia",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/BO.png"
+    },
+    {
+        id: 13,
+        countryCode: "BR",
+        name: "Brazil",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/BR.png"
+    },
+    {
+        id: 14,
+        countryCode: "BS",
+        name: "Bahamas",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/BS.png"
+    },
+    {
+        id: 15,
+        countryCode: "BW",
+        name: "Botswana",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/BW.png"
+    },
+    {
+        id: 16,
+        countryCode: "BY",
+        name: "Belarus",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/BY.png"
+    },
+    {
+        id: 17,
+        countryCode: "BZ",
+        name: "Belize",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/BZ.png"
+    },
+    {
+        id: 18,
+        countryCode: "CA",
+        name: "Canada",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/CA.png"
+    },
+    {
+        id: 19,
+        countryCode: "CH",
+        name: "Switzerland",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/CH.png"
+    },
+    {
+        id: 20,
+        countryCode: "CL",
+        name: "Chile",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/CL.png"
+    },
+    {
+        id: 21,
+        countryCode: "CN",
+        name: "China",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/CN.png"
+    },
+    {
+        id: 22,
+        countryCode: "CO",
+        name: "Colombia",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/CO.png"
+    },
+    {
+        id: 23,
+        countryCode: "CR",
+        name: "Costa Rica",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/CR.png"
+    },
+    {
+        id: 24,
+        countryCode: "CU",
+        name: "Cuba",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/CU.png"
+    },
+    {
+        id: 25,
+        countryCode: "CY",
+        name: "Cyprus",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/CY.png"
+    },
+    {
+        id: 26,
+        countryCode: "CZ",
+        name: "Czechia",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/CZ.png"
+    },
+    {
+        id: 27,
+        countryCode: "DE",
+        name: "Germany",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/DE.png"
+    },
+    {
+        id: 28,
+        countryCode: "DK",
+        name: "Denmark",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/DK.png"
+    },
+    {
+        id: 29,
+        countryCode: "DO",
+        name: "Dominican Republic",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/DO.png"
+    },
+    {
+        id: 30,
+        countryCode: "EC",
+        name: "Ecuador",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/EC.png"
+    },
+    {
+        id: 31,
+        countryCode: "EE",
+        name: "Estonia",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/EE.png"
+    },
+    {
+        id: 32,
+        countryCode: "EG",
+        name: "Egypt",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/EG.png"
+    },
+    {
+        id: 33,
+        countryCode: "ES",
+        name: "Spain",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/ES.png"
+    },
+    {
+        id: 34,
+        countryCode: "FI",
+        name: "Finland",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/FI.png"
+    },
+    {
+        id: 35,
+        countryCode: "FO",
+        name: "Faroe Islands",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/FO.png"
+    },
+    {
+        id: 36,
+        countryCode: "FR",
+        name: "France",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/FR.png"
+    },
+    {
+        id: 37,
+        countryCode: "GA",
+        name: "Gabon",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/GA.png"
+    },
+    {
+        id: 38,
+        countryCode: "GB",
+        name: "United Kingdom",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/GB.png"
+    },
+    {
+        id: 39,
+        countryCode: "GD",
+        name: "Grenada",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/GD.png"
+    },
+    {
+        id: 40,
+        countryCode: "GG",
+        name: "Guernsey",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/GG.png"
+    },
+    {
+        id: 41,
+        countryCode: "GI",
+        name: "Gibraltar",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/GI.png"
+    },
+    {
+        id: 42,
+        countryCode: "GL",
+        name: "Greenland",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/GL.png"
+    },
+    {
+        id: 43,
+        countryCode: "GM",
+        name: "Gambia",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/GM.png"
+    },
+    {
+        id: 44,
+        countryCode: "GR",
+        name: "Greece",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/GR.png"
+    },
+    {
+        id: 45,
+        countryCode: "GT",
+        name: "Guatemala",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/GT.png"
+    },
+    {
+        id: 46,
+        countryCode: "GY",
+        name: "Guyana",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/GY.png"
+    },
+    {
+        id: 47,
+        countryCode: "HN",
+        name: "Honduras",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/HN.png"
+    },
+    {
+        id: 48,
+        countryCode: "HR",
+        name: "Croatia",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/HR.png"
+    },
+    {
+        id: 49,
+        countryCode: "HT",
+        name: "Haiti",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/HT.png"
+    },
+    {
+        id: 50,
+        countryCode: "HU",
+        name: "Hungary",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/HU.png"
+    },
+    {
+        id: 51,
+        countryCode: "ID",
+        name: "Indonesia",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/ID.png"
+    },
+    {
+        id: 52,
+        countryCode: "IE",
+        name: "Ireland",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/IE.png"
+    },
+    {
+        id: 53,
+        countryCode: "IM",
+        name: "Isle of Man",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/IM.png"
+    },
+    {
+        id: 54,
+        countryCode: "IS",
+        name: "Iceland",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/IS.png"
+    },
+    {
+        id: 55,
+        countryCode: "IT",
+        name: "Italy",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/IT.png"
+    },
+    {
+        id: 56,
+        countryCode: "JE",
+        name: "Jersey",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/JE.png"
+    },
+    {
+        id: 57,
+        countryCode: "JM",
+        name: "Jamaica",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/JM.png"
+    },
+    {
+        id: 58,
+        countryCode: "JP",
+        name: "Japan",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/JP.png"
+    },
+    {
+        id: 59,
+        countryCode: "KR",
+        name: "South Korea",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/KR.png"
+    },
+    {
+        id: 60,
+        countryCode: "LI",
+        name: "Liechtenstein",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/LI.png"
+    },
+    {
+        id: 61,
+        countryCode: "LS",
+        name: "Lesotho",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/LS.png"
+    },
+    {
+        id: 62,
+        countryCode: "LT",
+        name: "Lithuania",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/LT.png"
+    },
+    {
+        id: 63,
+        countryCode: "LU",
+        name: "Luxembourg",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/LU.png"
+    },
+    {
+        id: 64,
+        countryCode: "LV",
+        name: "Latvia",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/LV.png"
+    },
+    {
+        id: 65,
+        countryCode: "MA",
+        name: "Morocco",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/MA.png"
+    },
+    {
+        id: 66,
+        countryCode: "MC",
+        name: "Monaco",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/MC.png"
+    },
+    {
+        id: 67,
+        countryCode: "MD",
+        name: "Moldova",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/MD.png"
+    },
+    {
+        id: 68,
+        countryCode: "ME",
+        name: "Montenegro",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/ME.png"
+    },
+    {
+        id: 69,
+        countryCode: "MG",
+        name: "Madagascar",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/MG.png"
+    },
+    {
+        id: 70,
+        countryCode: "MK",
+        name: "North Macedonia",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/MK.png"
+    },
+    {
+        id: 71,
+        countryCode: "MN",
+        name: "Mongolia",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/MN.png"
+    },
+    {
+        id: 72,
+        countryCode: "MS",
+        name: "Montserrat",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/MS.png"
+    },
+    {
+        id: 73,
+        countryCode: "MT",
+        name: "Malta",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/MT.png"
+    },
+    {
+        id: 74,
+        countryCode: "MX",
+        name: "Mexico",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/MX.png"
+    },
+    {
+        id: 75,
+        countryCode: "MZ",
+        name: "Mozambique",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/MZ.png"
+    },
+    {
+        id: 76,
+        countryCode: "NA",
+        name: "Namibia",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/NA.png"
+    },
+    {
+        id: 77,
+        countryCode: "NE",
+        name: "Niger",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/NE.png"
+    },
+    {
+        id: 78,
+        countryCode: "NG",
+        name: "Nigeria",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/NG.png"
+    },
+    {
+        id: 79,
+        countryCode: "NI",
+        name: "Nicaragua",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/NI.png"
+    },
+    {
+        id: 80,
+        countryCode: "NL",
+        name: "Netherlands",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/NL.png"
+    },
+    {
+        id: 81,
+        countryCode: "NO",
+        name: "Norway",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/NO.png"
+    },
+    {
+        id: 82,
+        countryCode: "NZ",
+        name: "New Zealand",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/NZ.png"
+    },
+    {
+        id: 83,
+        countryCode: "PA",
+        name: "Panama",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/PA.png"
+    },
+    {
+        id: 84,
+        countryCode: "PE",
+        name: "Peru",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/PE.png"
+    },
+    {
+        id: 85,
+        countryCode: "PG",
+        name: "Papua New Guinea",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/PG.png"
+    },
+    {
+        id: 86,
+        countryCode: "PL",
+        name: "Poland",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/PL.png"
+    },
+    {
+        id: 87,
+        countryCode: "PR",
+        name: "Puerto Rico",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/PR.png"
+    },
+    {
+        id: 88,
+        countryCode: "PT",
+        name: "Portugal",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/PT.png"
+    },
+    {
+        id: 89,
+        countryCode: "PY",
+        name: "Paraguay",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/PY.png"
+    },
+    {
+        id: 90,
+        countryCode: "RO",
+        name: "Romania",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/RO.png"
+    },
+    {
+        id: 91,
+        countryCode: "RS",
+        name: "Serbia",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/RS.png"
+    },
+    {
+        id: 92,
+        countryCode: "RU",
+        name: "Russia",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/RU.png"
+    },
+    {
+        id: 93,
+        countryCode: "SE",
+        name: "Sweden",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/SE.png"
+    },
+    {
+        id: 94,
+        countryCode: "SG",
+        name: "Singapore",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/SG.png"
+    },
+    {
+        id: 95,
+        countryCode: "SI",
+        name: "Slovenia",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/SI.png"
+    },
+    {
+        id: 96,
+        countryCode: "SJ",
+        name: "Svalbard and Jan Mayen",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/SJ.png"
+    },
+    {
+        id: 97,
+        countryCode: "SK",
+        name: "Slovakia",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/SK.png"
+    },
+    {
+        id: 98,
+        countryCode: "SM",
+        name: "San Marino",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/SM.png"
+    },
+    {
+        id: 99,
+        countryCode: "SR",
+        name: "Suriname",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/SR.png"
+    },
+    {
+        id: 100,
+        countryCode: "SV",
+        name: "El Salvador",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/SV.png"
+    },
+    {
+        id: 101,
+        countryCode: "TN",
+        name: "Tunisia",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/TN.png"
+    },
+    {
+        id: 102,
+        countryCode: "TR",
+        name: "Turkey",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/TR.png"
+    },
+    {
+        id: 103,
+        countryCode: "UA",
+        name: "Ukraine",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/UA.png"
+    },
+    {
+        id: 104,
+        countryCode: "US",
+        name: "United States",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/US.png"
+    },
+    {
+        id: 105,
+        countryCode: "UY",
+        name: "Uruguay",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/UY.png"
+    },
+    {
+        id: 106,
+        countryCode: "VA",
+        name: "Vatican City",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/VA.png"
+    },
+    {
+        id: 107,
+        countryCode: "VE",
+        name: "Venezuela",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/VE.png"
+    },
+    {
+        id: 108,
+        countryCode: "VN",
+        name: "Vietnam",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/VN.png"
+    },
+    {
+        id: 109,
+        countryCode: "ZA",
+        name: "South Africa",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/ZA.png"
+    },
+    {
+        id: 110,
+        countryCode: "ZW",
+        name: "Zimbabwe",
+        imageURL: "https://www.countryflagicons.com/FLAT/64/ZW.png"
+    }
+]
