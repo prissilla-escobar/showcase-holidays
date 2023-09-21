de61b2b01538adb72e894d6e9e91f2d773ada695
Prissilla Escobar <prissilla988@gmail.com>
2023-09-21 17:59:23 -0600

    Create await

--- src/api.js
+++ src/api.js
@@ -1,12 +1,24 @@
-export function getCountries() {
-    return fetch(`https://date.nager.at/api/v3/AvailableCountries`)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Trailer not found.')
-      }
-      return response.json()
-    })
-  }
+export async function getCountries() {
+    let Url = 'https://date.nager.at/api/v3/AvailableCountries'
+    try {
+        const response = await fetch(Url)
+        return (response.json())
+    }
+    catch(error) {
+        return error
+    }
+}
+
+
+// export function getCountries() {
+//     return fetch(`https://date.nager.at/api/v3/AvailableCountries`)
+//     .then(response => {
+//       if (!response.ok) {
+//         throw new Error('Country not found.')
+//       }
+//       return response.json()
+//     })
+//   }
 
   export function getCountryHolidays() {
     return fetch(`https://date.nager.at/api/v3/PublicHolidays/2023/US`)
