93ce5a35121d676627f9e4be6e233a7410146a61
Prissilla Escobar <prissilla988@gmail.com>
2023-09-21 17:57:24 -0600

    Create useState, useEffect and render All movies

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-import { getCountries, getCountryHolidays } from './api';
-import { useState, useEffect } from 'react';
+import './App.css'
+import { getCountries, getCountryHolidays } from './api'
+import { useState, useEffect } from 'react'
+import { Routes, Route } from 'react-router-dom'
+import AllCountries from './Components/AllCountries/AllCountries'
 
 function App() {
 
-  const [countries, setCountries] = useState('')
+  const [countries, setCountries] = useState([])
+  const [serverError, setServerError] = useState({hasError: false, message: ''})
 
   useEffect(() => {
     getCountries()
       .then(data => {
         setCountries(data)
       })
-  })
-
-  const codes = () => {
-    {countries.map((country) => {
-      return country
-    })}
-  }
-
+      .catch(error => {
+        setServerError({hasError: true, message: `${error.message}`})
+      })
+  }, [])
 
   return (
     <div className="App">
-      
+      <Routes>
+        <Route path='/' element={<AllCountries countries={countries} />} />
+      </Routes>
     </div>
   );
 }
