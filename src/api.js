796923965a5bff89fa9c1f79669e42edaeae8051
Prissilla Escobar <prissilla988@gmail.com>
2023-09-22 11:12:49 -0600

    Refactor holiday calls

--- src/api.js
+++ src/api.js
@@ -20,24 +20,23 @@ export async function getCountries() {
 //     })
 //   }
 
-  export function getCountryHolidays() {
-    return fetch(`https://date.nager.at/api/v3/PublicHolidays/2023/US`)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Country not found.')
-      }
-      return response.json()
-    })
-  } 
+  // export function getCountryHolidays(countryCode) {
+  //   return fetch(`https://date.nager.at/api/v3/PublicHolidays/2023/${countryCode}`)
+  //   .then(response => {
+  //     if (!response.ok) {
+  //       throw new Error('Country not found.')
+  //     }
+  //     return response.json()
+  //   })
+  // } 
 
-
-//   export function getCountryFlag() {
-//     return fetch(`https://flagsapi.com/BE/flat/64.png`)
-//     .then(response => {
-//       if (!response.ok) {
-//         throw new Error('Trailer not found.')
-//       }
-//       return response.json()
-//     })    
-//     .then(data => console.log(data))
-//   } 
+  export async function getCountryHolidays(countryCode) {
+    let Url = `https://date.nager.at/api/v3/PublicHolidays/2023/${countryCode}`
+    try {
+        const response = await fetch(Url)
+        return (response.json())
+    }
+    catch(error) {
+        return error
+    }
+}
