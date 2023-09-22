cd2ee530685531fc36962bbcf1ca7859abbab029
Prissilla Escobar <prissilla988@gmail.com>
2023-09-22 14:13:17 -0600

    Add Link to correct path when user clicks on a country

--- src/Components/CountryCard/CountryCard.js
+++ src/Components/CountryCard/CountryCard.js
@@ -1,5 +1,6 @@
 import './CountryCard.css'
 import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 import { countryFlags } from '../../countryEmoji'
 
 function Card({id, countryName, countryCode}) {
@@ -11,12 +12,14 @@ function Card({id, countryName, countryCode}) {
     })
     
     return (
-        <div className="country-card">
-            <img className={`${countryCode}-country-flag`} src={`${individualFlag.imageURL}`} alt={`image of ${countryName}'s flag`} />
-            <div className="country-name">
-                <h2>{countryName}</h2>
+        <Link to={`/2023/${countryCode}`}>
+            <div className="country-card">
+                <img className={`${countryCode}-country-flag`} src={`${individualFlag.imageURL}`} alt={`image of ${countryName}'s flag`} />
+                <div className="country-name">
+                    <h2>{countryName}</h2>
+                </div>
             </div>
-        </div>
+        </Link>
     )
 }
 
