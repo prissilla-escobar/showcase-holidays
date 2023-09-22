89e703d5b53709271c776bc85009775438047da8
Prissilla Escobar <prissilla988@gmail.com>
2023-09-22 15:08:23 -0600

    Create error page

---
+++ src/Components/ServerError/ServerError.js
@@ -0,0 +1,26 @@
+import './ServerError.css'
+import { Link } from 'react-router-dom'
+
+function Error({ resetError, message}) {
+    
+    return (
+        <div className='error-message'>
+            <img className="error-photo" src='https://images.wagwalkingweb.com/media/daily_wag/blog_articles/hero/1667345491.5716999/how-to-travel-with-a-dog-on-a-plane.png' />
+            <h2 className='oh-no'>OH NO!</h2>
+            <h2 className='message'>
+                {!message 
+                ? "The page you're looking for doesn't exist." 
+                : typeof message === 'string'
+                    ? message
+                    : message.message}
+            </h2>
+            <Link to='/' className='nav'>
+                <div onClick={resetError}>
+                <button className='home-link'>Please Return Home</button>
+                </div>
+            </Link>
+        </div>
+    )
+}
+
+export default Error
