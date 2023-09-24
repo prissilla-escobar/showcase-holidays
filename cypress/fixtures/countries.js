443fd85f7b273973087976b983968f5de5d780ab
Prissilla Escobar <prissilla988@gmail.com>
2023-09-23 18:49:46 -0600

    Add fixture file

---
+++ cypress/fixtures/countries.js
@@ -0,0 +1,26 @@
+[
+    {
+        "countryCode": "AD",
+        "name": "Andorra"
+    },
+    {
+        "countryCode": "AL",
+        "name": "Albania"
+    },
+    {
+        "countryCode": "EG",
+        "name": "Egypt"
+    },
+    {
+        "countryCode": "ES",
+        "name": "Spain"
+    },
+    {
+        "countryCode": "ZA",
+        "name": "South Africa"
+    },
+    {
+        "countryCode": "ZW",
+        "name": "Zimbabwe"
+    }
+]
